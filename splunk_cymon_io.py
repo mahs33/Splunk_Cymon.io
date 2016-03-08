8621d8fc2ba500a65753fff94e21669e16476a14
Simon <xg5-simon@users.noreply.github.com>
2016-03-08 14:53:41 +1100

    Update splunk_cymon_io.py
    Added API and domain support.

--- splunk_cymon_io.py
+++ splunk_cymon_io.py
@@ -6,12 +6,8 @@ __description__  = 'Quickly hacked together script to query cymon.io for a' \
 __requirements__ = 'Splunk'
 __author__       = 'xg5-simon'
 __email__        = 'simonlvgn at gmail com'
-__version__      = '0.0.1 alpha'
+__version__      = '0.0.2 alpha'
 __date__         = '13/01/2016'
-
-
-# TODO: Add support for api key
-# TODO: Add support for full cymon.io python library (https://github.com/eSentire/cymon-python)
 
 
 import re
@@ -22,13 +18,14 @@ import sys
 import string
 import requests
 import time
+import socket
 
 import splunk.Intersplunk
 
 
 API_KEY = ''
-HTTP_PROXY  = ""
-HTTPS_PROXY = ""
+HTTP_PROXY  = ''
+HTTPS_PROXY = ''
 
 
 class cymon(object):
@@ -43,17 +40,33 @@ class cymon(object):
             "http":  HTTP_PROXY,
             "https": HTTPS_PROXY,
         }
+        if auth_token:
+            self.session.headers.update({'Authorization': 'Token {0}'.format(auth_token)})
 
     def get(self, method, params=None):
         r = self.session.get(self.endpoint + method, params=params, proxies=self.session.proxies)
         r.raise_for_status()
         json = r.content
-		#print json
+                #print json
         return json
 
     def ip_lookup(self, ip_addr):
-        r = self.get('/ip/' + ip_addr)
+        r = self.get('/ip/' + ip_addr + '/events')
         return r
+
+    def domain_lookup(self, name):
+        r = self.get('/domain/' + name)
+        return r
+
+
+def validate_ip(ip_dom):
+    ''' Need to add better logic here. If variable passed to this function is
+        not a valid ipv4 it "assumes" it is a valid domain.'''
+    try:
+        socket.inet_aton(ip_dom)
+        return True
+    except:
+        return False
 
 
 def main():
@@ -62,15 +75,17 @@ def main():
         splunk.Intersplunk.parseError("No arguments provided, please provide an ip address or URL. E.g, ""| cymon __EXECUTE__ 8.8.8.8 | spath input=cy""")
         sys.exit(0)
 
-    api = cymon()
+    api = cymon(API_KEY)
+    search_obj = sys.argv[1]
+    query_obj = validate_ip(search_obj)
 
-    search_ip = sys.argv[1]
-    result_json = api.ip_lookup(search_ip)
-    #print result_json
+    if query_obj == True:
+        result_json = api.ip_lookup(search_obj)
+    else:
+        result_json = api.domain_lookup(search_obj)
+
     output = csv.writer(sys.stdout)
     data = [['cy'],[result_json]]
-    #print data
     output.writerows(data)
-
 
 main()
